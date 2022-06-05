5c38eb684c1d0002f4296d3144209d8eafa3d8ad
baoviettk <66452636+baoviettk@users.noreply.github.com>
2022-06-05 19:29:51 -0400

    Time: 78 ms (54.96%), Space: 16.8 MB (76.10%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def insert(self, intervals, newInterval):
-        if len(intervals)==0:
-            return [newInterval]
-        if intervals[0][0]>newInterval[1]:
-            intervals.insert(0,newInterval)
-            return intervals
-        if intervals[-1][1]< newInterval[0]:
-            intervals.append(newInterval)
-            return intervals
+#         if len(intervals)==0:
+#             return [newInterval]
+#         if intervals[0][0]>newInterval[1]:
+#             intervals.insert(0,newInterval)
+#             return intervals
+#         if intervals[-1][1]< newInterval[0]:
+#             intervals.append(newInterval)
+#             return intervals
         
         arr=[]
         for i in range(len(intervals)):
