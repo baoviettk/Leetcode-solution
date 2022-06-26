29ee1b82e4e2ce9f01f88891e1fbc737591574aa
baoviettk <66452636+baoviettk@users.noreply.github.com>
2022-06-26 19:59:16 -0400

    Time: 1786 ms (58.33%), Space: 37.7 MB (83.33%) - LeetHub

--- 502-ipo/502-ipo.py
+++ 502-ipo/502-ipo.py
@@ -2,16 +2,15 @@ class Solution(object):
     def findMaximizedCapital(self, k, w, profits, capital):
         min_capital=[]
         max_profit=[]
-        current_w=w
         for i in range(len(capital)):
             heapq.heappush(min_capital,(capital[i],i))
         for _ in range(k):
-            while min_capital and min_capital[0][0]<=current_w:
+            while min_capital and min_capital[0][0]<=w:
                 cur_cap,i=heapq.heappop(min_capital)
                 heapq.heappush(max_profit,(-profits[i]))
             if not max_profit:
                 break
-            current_w+=-heapq.heappop(max_profit)
+            w+=-heapq.heappop(max_profit)
             
-        return current_w
+        return w
         
