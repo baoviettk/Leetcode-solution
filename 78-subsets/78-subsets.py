08482ad7d85a2a4fe3bb1b5bc556866cd1976aff
baoviettk <66452636+baoviettk@users.noreply.github.com>
2022-06-27 19:20:08 -0400

    Time: 22 ms (82.84%), Space: 13.4 MB (89.75%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def subsets(self, nums):
+        cur, arr=[],[]
+        n=len(nums)
+        
+        def dfs(i):
+            if i>=n:
+                arr.append(list(cur))
+                return
+            
+            cur.append(nums[i])
+            dfs(i+1)
+            
+            cur.pop()
+            dfs(i+1)
+        
+        dfs(0)
+        return arr
+        
