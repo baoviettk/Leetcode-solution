c6f3242dd41fe3cf58ccfb7a2c564122406c88b5
baoviettk <66452636+baoviettk@users.noreply.github.com>
2022-08-04 18:56:10 -0400

    Time: 82 ms (17.98%), Space: 14.3 MB (18.83%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -7,16 +7,12 @@ class Solution:
                 return m
             if nums[m]>=nums[l]:
                 if target<=nums[m] and target>=nums[l]:
-                    print("take left")
                     r=m-1
                 else:
-                    print("take right")
                     l=m+1
             else:
                 if target>=nums[m] and target<nums[l]:
-                    print("take right")
                     l=m+1
                 else:
-                    print("take left")
                     r=m-1
         return -1
