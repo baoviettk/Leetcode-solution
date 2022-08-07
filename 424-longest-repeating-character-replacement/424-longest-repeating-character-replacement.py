3e946db319896669b15311cb8a568e01109ec03d
baoviettk <66452636+baoviettk@users.noreply.github.com>
2022-08-07 01:18:15 -0400

    Time: 630 ms (5.94%), Space: 14.2 MB (19.99%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -5,15 +5,24 @@ class Solution:
         cur=1
         dic={}
         dic[s[0]]=1
+        most_char=s[0]
         for i in range(1,len(s)):
+            print("char:"+str(s[i]))
+            print(most_char)
             cur+=1
             if s[i] not in dic.keys():
                 dic[s[i]]=0
+                print(i)
             dic[s[i]]+=1
-            while cur-max(dic.values())>k:
+            print(dic)
+            if most_char!=s[i]:
+                if dic[most_char]<dic[s[i]]:
+                    most_char=s[i]
+            if cur-dic[most_char]>k:
                 dic[s[l]]-=1
                 l+=1
                 cur-=1
+                print("cur:"+str(cur))
             result=max(result,cur)
         
         return result
