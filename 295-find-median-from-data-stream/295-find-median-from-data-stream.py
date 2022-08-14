0d3e316b3ce2a457f5842e4b2605be40454997ae
baoviettk <66452636+baoviettk@users.noreply.github.com>
2022-08-14 18:50:29 -0400

    Time: 681 ms (75.07%), Space: 35.6 MB (88.76%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -1,36 +1,25 @@
-class MedianFinder(object):
+class MedianFinder:
 
     def __init__(self):
-        self.small, self.large=[],[]
+        #min for bigger half, max for smaller half
+        self.max_heap, self.min_heap=[],[]
+        
 
-        # small is max
-        # large is min
-    def addNum(self, num):
-        heapq.heappush(self.small,-1*num)
-        
-        if self.small and self.large and -1*self.small[0]>self.large[0]:
-            val=-1*heapq.heappop(self.small)            
-            heapq.heappush(self.large,val)
-        
-        if len(self.small)> len(self.large)+1:
-            val=-1*heapq.heappop(self.small)            
-            heapq.heappush(self.large,val)
-        
-        if len(self.large)> len(self.small)+1:
-            val=heapq.heappop(self.large)            
-            heapq.heappush(self.small,-1*val)
+    def addNum(self, num: int) -> None:
+        heappush(self.max_heap,-num)
+        if len(self.max_heap)>len(self.min_heap)+1:
+            heappush(self.min_heap, -heappop(self.max_heap))
+        if len(self.min_heap)>0 and -self.max_heap[0]>self.min_heap[0]:
+            heappush(self.max_heap,-heappop(self.min_heap))
+            heappush(self.min_heap,-heappop(self.max_heap))
+
             
-
-    def findMedian(self):
-        if len(self.small)>len(self.large):
-            return -1*self.small[0]
-        if len(self.small)<len(self.large):
-            return self.large[0]
-        return (-1*self.small[0]+ self.large[0])/float(2)
         
 
+    def findMedian(self) -> float:
+        if len(self.max_heap)>len(self.min_heap):
+            return -self.max_heap[0]
+        else:
+            return (-self.max_heap[0]+self.min_heap[0])/2
 
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
+
