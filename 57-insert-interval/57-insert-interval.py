659a0cef7d00004c029dd967f642ea672265cbd0
baoviettk <66452636+baoviettk@users.noreply.github.com>
2022-08-15 14:52:30 -0400

    Time: 131 ms (45.83%), Space: 17.4 MB (53.11%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,25 +1,13 @@
-class Solution(object):
-    def insert(self, intervals, newInterval):
-#         if len(intervals)==0:
-#             return [newInterval]
-#         if intervals[0][0]>newInterval[1]:
-#             intervals.insert(0,newInterval)
-#             return intervals
-#         if intervals[-1][1]< newInterval[0]:
-#             intervals.append(newInterval)
-#             return intervals
-        
-        arr=[]
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        result=[]
         for i in range(len(intervals)):
-            if intervals[i][0]>newInterval[1]:
-                arr.append(newInterval)
-                return arr+ intervals[i:]
-            if intervals[i][1]<newInterval[0]:
-                arr.append(intervals[i])
-                
+            if newInterval[1]<intervals[i][0]:
+                result.append(newInterval)
+                return result+intervals[i:]
+            elif newInterval[0]>intervals[i][1]:
+                result.append(intervals[i])
             else:
-                print('here')
-                newInterval= [min(intervals[i][0], newInterval[0]),max(intervals[i][1], newInterval[1])]
-                
-        arr.append(newInterval)
-        return arr
+                newInterval=[min(newInterval[0],intervals[i][0]), max(newInterval[1],intervals[i][1])]
+        result.append(newInterval)
+        return result
