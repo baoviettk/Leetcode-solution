7d39fabd5e62277b8df052ad97fe0b191301fd39
baoviettk <66452636+baoviettk@users.noreply.github.com>
2022-08-28 17:10:31 -0400

    Time: 239 ms (6.72%), Space: 13.5 MB (68.91%) - LeetHub

---
+++ 909-snakes-and-ladders/909-snakes-and-ladders.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def snakesAndLadders(self, board):
+        board.reverse()
+        l=len(board)
+
+        def strc(square):
+            r=(square-1)//l
+            c=(square-1)%l
+            if r%2:
+                c=l-c-1
+            return [r,c]
+            
+        q= deque() #[square, cost]
+        result=0
+        visit=set()
+        q.append([1,0])
+        
+        while q:
+            sq,co=q.popleft()
+            for i in range(1,7):
+                nxt_sq=sq+i
+                r,c=strc(nxt_sq)
+                if board[r][c]!=-1:
+                    nxt_sq=board[r][c]
+                if nxt_sq==l*l:
+                    return co+1
+                if nxt_sq not in visit:
+                    visit.add(nxt_sq)
+                    q.append([nxt_sq,co+1])
+        return -1
+            
+        
+        
+        
+        
+        
