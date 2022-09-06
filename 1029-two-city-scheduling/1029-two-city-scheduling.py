a183beaf70343aa3eefbe91e903ed36ea24bf31d
baoviettk <66452636+baoviettk@users.noreply.github.com>
2022-09-06 19:44:45 -0400

    Time: 43 ms (91.46%), Space: 14 MB (10.64%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        dif=[]
+        result=0
+        LEN=len(costs)
+        for i in range(LEN):
+            dif.append([costs[i][1]-costs[i][0], i])
+        dif.sort()
+        for i in range(LEN//2):
+            result+=costs[dif[i][1]][1]
+        for i in range(LEN//2, LEN):
+            result+=costs[dif[i][1]][0]
+            
+        return result
