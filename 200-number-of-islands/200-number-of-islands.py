66d57ded9802951ee02f7f3371e7b5874a6adc13
baoviettk <66452636+baoviettk@users.noreply.github.com>
2022-09-13 00:35:39 -0400

    Time: 562 ms (35.13%), Space: 16.4 MB (81.37%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,23 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        max_x,max_y=len(grid), len(grid[0])
+        MAX_ROW, MAX_COL= len(grid), len(grid[0])
+        
+        def mark(r,c):
+            if r<MAX_ROW and c<MAX_COL and r>-1 and c>-1 and grid[r][c]=="1":
+                grid[r][c]="2"
+                mark(r+1,c)
+                mark(r-1,c)
+                mark(r,c+1)
+                mark(r,c-1)
+            return
         result=0
-        
-        def markVisited(i,j):
-            if i>=max_x or j>=max_y or i<0 or j<0 or grid[i][j]!="1":
-                return
-            grid[i][j]="2"
-            markVisited(i-1,j)
-            markVisited(i+1,j)
-            markVisited(i,j-1)
-            markVisited(i,j+1)
-            
-        
-        for i in range(max_x):
-            for j in range(max_y):
+        for i in range(MAX_ROW):
+            for j in range(MAX_COL):
                 if grid[i][j]=="1":
-                    markVisited(i,j)
                     result+=1
-                
+                    mark(i,j)
         return result
                     
