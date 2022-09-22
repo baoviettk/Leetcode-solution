1a359f10bca0f014a453239d7a482b371d6e1d88
baoviettk <66452636+baoviettk@users.noreply.github.com>
2022-09-22 02:05:50 -0400

    Time: 201 ms (23.91%), Space: 15.8 MB (30.34%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -1,12 +1,13 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        s+="+"
         stack,cur_num,ope=[],0,"+"
         all_oper={"+","-","*", "/"}
         for i in range(len(s)):
             char=s[i]
             if char.isnumeric():
                 cur_num=cur_num*10+int(char)
-            if char in all_oper or i ==len(s)-1:
+            elif char in all_oper:
                 if ope=="+":
                     stack.append(cur_num)
                 elif ope=="-":
