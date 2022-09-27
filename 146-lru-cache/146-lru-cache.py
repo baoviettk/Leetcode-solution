d5c533075f69289a36803225573adc562fa4eda7
baoviettk <66452636+baoviettk@users.noreply.github.com>
2022-09-27 19:59:53 -0400

    Time: 1228 ms (66.35%), Space: 75 MB (84.05%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,45 +1,53 @@
 class Node:
-    
-    def __init__(self,key:int, val:int):
-        self.key=key
+    def __init__(self,key, val):
         self.val=val
-        self.next, self.prev=None, None
+        self.key=key
+        self.prev=None
+        self.next=None
+        
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap=capacity
-        self.cache={}
-        
+        self.capacity=capacity
+        self.dic={}
         self.left,self.right=Node(0,0), Node(0,0)
-        self.left.next, self.right.prev=self.right, self.left
+        self.left.next=self.right
+        self.right.prev=self.left
         
-    def add(self, node: Node): #to the right
-        prev,right=self.right.prev, self.right
-        prev.next, node.next=node,right
-        node.prev, right.prev=prev,node
 
-    def remove(self, node:Node):
-        prev, right=node.prev, node.next
-        prev.next, right.prev=right, prev
-    
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        self.remove(self.cache[key])
-        self.add(self.cache[key])
-        return self.cache[key].val
-    
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key]=Node(key,value)
-        self.add(self.cache[key])
-        if len(self.cache)>self.cap:
-            remove_node=self.left.next
-            del self.cache[remove_node.key]
-            self.remove(remove_node)
+        if key in self.dic:
+            val=self.dic[key].val
+            self.delete(key)
+            self.add(key,val)
+            return self.dic[key].val
+        return -1
         
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            self.delete(key)
+        self.add(key,value)
+        if len(self.dic)>self.capacity:
+            self.delete(self.left.next.key)
+        
+    def delete(self, key):
+        cur=self.dic[key]
+        prev,nxt=cur.prev,cur.next
+        prev.next=nxt
+        nxt.prev=prev
+        del self.dic[key]
+    
+    def add(self, key, val):
+        new=Node(key,val)
+        prev=self.right.prev
+        self.right.prev=new
+        new.prev,new.next=prev,self.right
+        prev.next=new
+        new.next
+        self.dic[key]=new
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
