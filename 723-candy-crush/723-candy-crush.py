1a63bebd5aa540407b2e8188324c6dae773039dc
baoviettk <66452636+baoviettk@users.noreply.github.com>
2022-10-05 01:46:45 -0400

    Time: 775 ms (7.54%), Space: 14.1 MB (54.61%) - LeetHub

--- 723-candy-crush/723-candy-crush.py
+++ 723-candy-crush/723-candy-crush.py
@@ -1,42 +1,39 @@
 class Solution:
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
-        if not board:
-            return board
+        COL, ROW=len(board[0]),len(board)
         done=False
         while not done:
             done=True
-            #Crush row
-            for r in range(len(board)):
-                for c in range(len(board[0])-2):
-                    num1= abs(board[r][c])
-                    num2= abs(board[r][c+1])
-                    num3= abs(board[r][c+2])
-                    if  num1==num2 and num2==num3 and num1!=0:
-                        board[r][c]=-num1
-                        board[r][c+1]=-num2
-                        board[r][c+2]=-num3
+#             find horizontal
+            if COL>2:
+                for row in range(ROW):
+                    for x in range(COL-2):
+                        if abs(board[row][x])==abs(board[row][x+1]) and abs(board[row][x+1])==abs(board[row][x+2]) and board[row][x]!=0:
+                            val=-abs(board[row][x])
+                            board[row][x]=val
+                            board[row][x+1]=val
+                            board[row][x+2]=val
+                            done=False
+#           find vertical
+            for col in range(COL):
+                for y in range(ROW-2):
+                    if abs(board[y][col])==abs(board[y+1][col]) and abs(board[y+1][col])==abs(board[y+2][col]) and board[y][col]!=0:
+                        val=-abs(board[y][col])
+                        board[y][col]=val
+                        board[y+1][col]=val
+                        board[y+2][col]=val
                         done=False
-            #Crush column
-            for c in range(len(board[0])):
-                for r in range(len(board)-2):
-                    num1= abs(board[r][c])
-                    num2= abs(board[r+1][c])
-                    num3= abs(board[r+2][c])
-                    if  num1==num2 and num2==num3 and num1!=0:
-                        board[r][c]=-num1
-                        board[r+1][c]=-num2
-                        board[r+2][c]=-num3
-                        done=False
-            #Gravity
-            for c in range(len(board[0])):
-                replace=len(board)-1
-                for r in range(len(board)-1,-1,-1):
-                    if board[r][c]>0:
-                        board[replace][c]= board[r][c]
-                        replace-=1
-                for r in range(replace,-1,-1):
-                    board[r][c]=0
-                    
+#            Drop
+            for x in range(COL):
+                hole=ROW-1
+                for y in range(ROW-1,-1,-1):
+                    if board[y][x]<=0:
+                        continue
+                    board[y][x],board[hole][x]=board[hole][x],board[y][x]
+                    hole-=1
+                for y in range(0,hole+1):
+                    board[y][x]=0
+                
         return board
-            
-                    
+                
+                
